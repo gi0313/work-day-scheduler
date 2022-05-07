9e180dcc201a6a2645d1a99123fd4358ccf488f4
Giovanni Claro <gio.claro316@gmail.com>
2022-05-07 17:58:31 -0400

    added click event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,61 @@
-let time_div = $("#currentDay")
+const time_div = $("#currentDay")
 time_div.text(moment().format("dddd, MMMM Do, YYYY"));
 
-let textArea = $("#text-area")
-console.log(textArea.val());
+const nineAm = $("#9am")
+const tenAm = $("#10am")
+const elevenAm = $("#11am")
+const twelveAm = $("#12am")
+const onePm = $("#1pm")
+const twoPm = $("#2pm")
+const threePm = $("#3pm")
+const fourPm = $("#4pm")
+const fivePm = $("#5pm")
 
-let button = $("#save_button")
-button.on("click", function(){
-    console.log("button click")
-    localStorage.setItem(1, textArea.val())
-});
+var timeArray = [
+    nineAm,
+    tenAm,
+    elevenAm,
+    twelveAm,
+    onePm,
+    twoPm,
+    threePm,
+    fourPm,
+    fivePm
+]
+
+
+let textArea = $(".text-area")
+//console.log(textArea.val());
+
+let button = $(".save_button")
 
 function getFromLocalStorage() {
-    textArea.text = localStorage.getItem("1")
+    textArea.text = localStorage.getItem("")
 }
 
-getFromLocalStorage();
+function init() {
+    getFromLocalStorage()
+
+for (let i = 0; i < timeArray.length; i++) {
+    const element = timeArray[i];
+    //console.log(element);
+    
+    $(element).on("click", function(event){
+        //console.log(event.target.className);
+        if (event.target.className == "col-mb-2 save_button") {
+            //console.log("click");
+            var eventBlock = $(event.target).siblings()
+            var timeKey = $(event.target).parent().parent().attr("id")
+            //console.log(eventBlock[1].value)
+            //console.log(timeKey)
+            localStorage.setItem(timeKey, eventBlock[1].value)
+        }
+    })
+    //$("#"+timeKey).children().children().siblings()
+    console.log($("#"+timeArray).children().children().siblings())
+    localStorage.getItem(timeKey)
+    //use time array
+}
+}
+
+init();
