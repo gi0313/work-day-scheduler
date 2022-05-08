afd98a9bc9c80d6d5c29e8428ed73345fbeb349c
Giovanni Claro <gio.claro316@gmail.com>
2022-05-08 15:19:58 -0400

    fixed html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-const time_div = $("#currentDay")
-time_div.text(moment().format("dddd, MMMM Do, YYYY"));
+//displays current day
+const date = $("#currentDay")
+date.text(moment().format("dddd, MMMM Do, YYYY"));
 
 const nineAm = $("#9am")
 const tenAm = $("#10am")
@@ -23,11 +24,10 @@ var timeArray = [
     fivePm
 ]
 
-
 let textArea = $(".text-area")
-//console.log(textArea.val());
+console.log(textArea.val());
 
-let button = $(".save_button")
+let button = $(".saveBtn")
 
 function getFromLocalStorage() {
     textArea.text = localStorage.getItem("")
@@ -38,22 +38,12 @@ function init() {
 
 for (let i = 0; i < timeArray.length; i++) {
     const element = timeArray[i];
-    //console.log(element);
+    console.log(element);
     
-    $(element).on("click", function(event){
-        //console.log(event.target.className);
-        if (event.target.className == "col-mb-2 save_button") {
-            //console.log("click");
-            var eventBlock = $(event.target).siblings()
-            var timeKey = $(event.target).parent().parent().attr("id")
-            //console.log(eventBlock[1].value)
-            //console.log(timeKey)
-            localStorage.setItem(timeKey, eventBlock[1].value)
-        }
-    })
-    //$("#"+timeKey).children().children().siblings()
-    console.log($("#"+timeArray).children().children().siblings())
-    localStorage.getItem(timeKey)
+
+    $(timeArray).children().val
+    //console.log($("#"+timeArray).children().children().siblings())
+    localStorage.getItem(timeArray)
     //use time array
 }
 }
