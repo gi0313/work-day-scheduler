85d3eb1abf5ebf399afde12de6d08ba15628eeef
Giovanni Claro <gio.claro316@gmail.com>
2022-05-08 23:45:52 -0400

    reworked entire javascript

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # work-day-scheduler
+
+## Task Day Scheduler
+A simple calendar appl that lets a user save events for each hour of the work day.
+
+## Made with
+
+* Html
+* Css
+* Javascript
+* Node.Js
+* jQuery
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,53 @@
-//displays current day
+//displays current day and time
 const date = $("#currentDay")
 date.text(moment().format("dddd, MMMM Do, YYYY"));
 
-const nineAm = $("#9am")
-const tenAm = $("#10am")
-const elevenAm = $("#11am")
-const twelveAm = $("#12am")
-const onePm = $("#1pm")
-const twoPm = $("#2pm")
-const threePm = $("#3pm")
-const fourPm = $("#4pm")
-const fivePm = $("#5pm")
 
-var timeArray = [
-    nineAm,
-    tenAm,
-    elevenAm,
-    twelveAm,
-    onePm,
-    twoPm,
-    threePm,
-    fourPm,
-    fivePm
-]
+$(".saveBtn").on("click", function () {
+    var taskText = $(this).parent(".text-area").val(); 
+    var taskTime = $(this).parent("id").val();
 
-let textArea = $(".text-area")
-console.log(textArea.val());
+    //set local storage 
+    localStorage.setItem(taskTime, taskText);
+})
 
-let button = $(".saveBtn")
+$("#9am .text-area").val(localStorage.getItem("9am"));
+$("#10am .text-area").val(localStorage.getItem("10am"));
+$("#11am .text-area").val(localStorage.getItem("11am"));
+$("#12am .text-area").val(localStorage.getItem("12am")); //not working?
+$("#1pm .text-area").val(localStorage.getItem("13pm"));
+$("#2pm .text-area").val(localStorage.getItem("14pm"));
+$("#3pm .text-area").val(localStorage.getItem("15pm"));
+$("#4pm .text-area").val(localStorage.getItem("16pm"));
+$("#5pm .text-area").val(localStorage.getItem("17pm"));
 
-function getFromLocalStorage() {
-    textArea.text = localStorage.getItem("")
+
+function currentTime() {
+    //get current number of hours
+    var currentTime = moment().hour(); 
+
+    //loop over time blocks???
+    $(".time-block").each(function() {
+        var currentHour = parseInt($(this));
+
+        //if the time is past or not and adds the css style
+         if (currentHour < currentTime) {
+             $(this).addClass("past");
+             $(this).removeClass("future");
+             $(this).removeClass("present");
+             console.log(currentTime);
+         }
+        
+         else if (currentHour === currentTime) {
+             $(this).removeClass("past");
+             $(this).addClass("present");
+             $(this).removeClass("future");
+         }
+         else {
+             $(this).removeClass("present");
+             $(this).removeClass("past");
+             $(this).addClass("future");
+         }
+     })
 }
-
-function init() {
-    getFromLocalStorage()
-
-for (let i = 0; i < timeArray.length; i++) {
-    const element = timeArray[i];
-    console.log(element);
-    
-
-    $(timeArray).children().val
-    //console.log($("#"+timeArray).children().children().siblings())
-    localStorage.getItem(timeArray)
-    //use time array
-}
-}
-
-init();
+currentTime();
